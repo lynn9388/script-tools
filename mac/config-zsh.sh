90151f76d8b9027edf2a62894d147c1dba991f6b
Lynn <lynn9388@gmail.com>
2019-10-10 17:31:19 +0800

    Add shell script to configure zsh

---
+++ mac/config-zsh.sh
@@ -0,0 +1,49 @@
+#!/bin/zsh
+
+mkdir -p ~/.zsh
+cd ~/.zsh
+
+curl -O https://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore
+curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
+git config --global user.name Lynn
+git config --global user.email lynn9388@gmail.com
+git config --global merge.conflictStyle diff3
+git config --global gpg.program gpg2
+git config --global user.signingkey EC6618DB26F0A0B2
+git config --global commit.gpgsign true
+git config --global http.proxy socks5h://localhost:1090
+git config --global core.excludesfile ~/.zsh/macOS.gitignore
+
+cat <<EOT >> ~/.zshrc
+HISTFILE=~/.zsh/.zsh_history
+
+############################## Git ##############################
+zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bash
+fpath=(~/.zsh /usr/local/share/zsh/site-functions /usr/share/zsh/site-functions /usr/share/zsh/5.7.1/functions)
+autoload -Uz compinit && compinit -d ~/.zsh/.zcompdump
+
+source ~/.zsh/git-prompt.sh
+
+export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWSTASHSTATE=1
+export GIT_PS1_SHOWUNTRACKEDFILES=1
+export GIT_PS1_SHOWUPSTREAM="auto"
+
+autoload -U colors && colors
+setopt PROMPT_SUBST ; PS1='%{\$fg[red]%}%n%{\$fg[green]%}\$(__git_ps1 " (%s)") %{\$fg[blue]%}%c $%{\$reset_color%} '
+############################## Git ##############################
+
+alias heroku1="cd /Users/lynn/Documents/Coding/heroku/1/;./heroku"
+alias heroku2="cd /Users/lynn/Documents/Coding/heroku/2/;./heroku"
+alias heroku3="cd /Users/lynn/Documents/Coding/heroku/3/;./heroku"
+alias gitl="git log --oneline --graph"
+alias gits="git status"
+
+alias ping2.2="ping 192.168.2.2"
+alias ping2.3="ping 192.168.2.3"
+alias ssh2.2="ssh -D 1091 lynn@192.168.2.2"
+alias ssh2.3="ssh lynn@192.168.2.3"
+EOT
